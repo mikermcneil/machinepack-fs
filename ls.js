0ed1d044acecd0fcf44fca630bf560e8aa2a7791
Mike McNeil <mike@balderdash.co>
2014-07-28 07:28:59 -0500

    Dont use deps

--- ls.js
+++ ls.js
@@ -1,21 +1,17 @@
 /**
- * --------------------------------------------------
- * Usage:
- *
- * require('node-machine')
- * .load('machinepack-fs/ls')
- * .configure({dir:'./'})
- * .exec(console.log)
+ * Module dependencies
  */
+
+var ls = require('list-directory-contents');
+
+
+
 
 module.exports = {
   id: 'ls',
   moduleName: 'machinepack-fs',
   description: 'List directory contents',
   transparent: true,
-  dependencies: {
-    'list-directory-contents': '*'
-  },
   inputs: {
     dir: {
       example: '/Users/mikermcneil/.tmp/foo'
@@ -27,14 +23,14 @@ module.exports = {
       example: [
         '/Users/mikermcneil/.tmp/foo/.gitignore',
         '/Users/mikermcneil/.tmp/foo/README.md',
+        '/Users/mikermcneil/.tmp/foo/bar/',
         '/Users/mikermcneil/.tmp/foo/bar/index.html',
         '/Users/mikermcneil/.tmp/foo/bar/favicon.ico',
         '/Users/mikermcneil/.tmp/foo/bar/images/logo.png'
       ]
     }
   },
-  fn: function ($i,$x,$d) {
-    var ls = $d['list-directory-contents'];
+  fn: function ($i,$x) {
     ls($i.dir, $x);
   }
 };
