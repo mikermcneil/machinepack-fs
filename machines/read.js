5db78dbafa5f1997bbb174bd0ba502ee305d1fc3
Mike McNeil <mike@balderdash.co>
2015-05-05 08:23:42 -0500

    Minor improvements to 'read' machine.

--- machines/read.js
+++ machines/read.js
@@ -1,44 +1,61 @@
 module.exports = {
+
+
   friendlyName: 'Read file',
-  description: 'Read file located at source path on disk into a string.',
-  extendedDescription: 'Assumes file is encoded using utf8.',
+
+
+  description: 'Read a file on disk as a string.',
+
+
+  extendedDescription: 'Assumes file contents are encoded using utf8.',
+
+
   cacheable: true,
 
+
+  idempotent: true,
+
+
   inputs: {
+
     source: {
       description: 'Absolute path to the source file (if relative path is provided, will resolve path from current working directory)',
       example: '/Users/mikermcneil/.tmp/foo',
       required: true
     }
+
   },
 
-  defaultExit: 'success',
-  catchallExit: 'error',
 
   exits: {
-    error: {},
+
     doesNotExist: {
       description: 'No file exists at the provided `source` path'
     },
+
     success: {
       example: 'stuff in a file!',
       description: 'Returns the contents of the file at `source` path'
     }
+
   },
+
 
   fn: function (inputs, exits) {
 
     var fs = require('fs');
 
     fs.readFile(inputs.source, 'utf8', function (err, contents) {
-      if (err) {
-        if (typeof err === 'object' && err.code === 'ENOENT') {
-          return exits.doesNotExist();
-        }
-        // Some unrecognized error
-        return exits.error(err);
+      // It worked!
+      if (!err) {
+        return exits.success(contents);
       }
-      return exits.success(contents);
+      // No need for `null` check here because we already know `err` is falsy
+      if (typeof err === 'object' && err.code === 'ENOENT') {
+        return exits.doesNotExist();
+      }
+      // Some unrecognized error
+      return exits.error(err);
     });
   }
 };
