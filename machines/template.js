3272407d9a60de8109edc0647a52497a5c2c74ef
Mike McNeil <mike@balderdash.co>
2015-05-10 08:56:15 -0500

    Improve metadata, make exits finer grained.

--- machines/template.js
+++ machines/template.js
@@ -1,18 +1,29 @@
 module.exports = {
 
+
   friendlyName: 'Template file',
+
+
   description: 'Read file at source path, replace substrings with provided data, then write to destination path.',
+
+
   extendedDescription: 'Uses lodash template syntax (e.g. `"Hi there, <%=firstName%>!"`)',
 
+
   inputs: {
+
     source: {
-      example: '/Users/mikermcneil/.tmp/foo',
+      description: 'The path on disk to the template file.',
+      example: '/Users/mikermcneil/.tmp/foo.tpl',
       required: true
     },
+
     destination: {
-      example: '/Users/mikermcneil/.tmp/bar',
+      description: 'The path on disk where the resulting file should be written',
+      example: '/Users/mikermcneil/.tmp/bar.md',
       required: true
     },
+
     data: {
       typeclass: 'dictionary'
       // e.g. {
@@ -23,92 +34,110 @@ module.exports = {
       //   projectName: 'Bikrosoft (Confidential)'
       // }
     },
+
     force: {
-      description: 'overwrite existing file(s)?',
-      example: false
+      friendlyName: 'Force?',
+      description: 'Whether or not to overwrite existing file(s).',
+      example: false,
+      defaultsTo: false
     },
-    options: {
-      description: 'template options (see http://lodash.com/docs#template)',
-      typeclass: 'dictionary'
-      // example: {
-      //   interpolate: /{{([\s\S]+?)}}/g
-      // }
+
+    escapeHTMLEntities: {
+      description: 'Whether or not to escape HTML entities',
+      example: false,
+      defaultsTo: false
     }
+
   },
 
-  defaultExit: 'success',
 
   exits: {
-    error: {
-      description: 'Unexpected error occurred.'
-    },
+
     success: {
       description: 'File written successfully.'
     },
+
     noTemplate: {
       description: 'Source template file not found'
     },
+
     missingData: {
-      example: {
-        status: 500,
-        exit: 'missingData',
-        name: 'ReferenceError',
-        message: '`appname` is used in template "/Users/mikermcneil/.tmp/foo", but no value for `appname` was provided as template data.'
-      }
+      friendlyName: 'missing data',
+      description: 'One or more variables used in the template were not provided in the template data.',
+
     }
+
   },
+
 
   fn: function (inputs, exits) {
 
     var util = require('util');
     var fsx = require('fs-extra');
     var _ = require('lodash');
+
     var writeFileFromStr = require('machine').build(require('./write'));
 
+    // Read template from disk
     fsx.readFile(inputs.source, 'utf8', function(err, contents) {
       if (err) {
         err = (typeof err === 'object' && err instanceof Error) ? err : new Error(err);
         err.message = 'Template error: ' + err.message;
         err.path = inputs.source;
         if (err.code === 'ENOENT') {
-          return (exits.noTemplate||exits.error)(err);
+          return (exits.noTemplate)(err);
         }
         return exits.error(err);
       }
-      try {
-        var options = inputs.options || {};
-        contents = _.template(contents, inputs.data||{}, options);
 
+      // Render template and data into a single string using Lodash
+      var result;
+      try {
+        result = _.template(contents, inputs.data||{}, {
+
+          // Provide access to `util` and `_` libs from within template.
+          imports: {
+            util: util,
+            _: _
+          }
+
+        });
+      } catch (e) {
+
+        // Recognize lodash template error (scope variable not defined)
+        var isTplError = _.isObject(e) && (e.name === 'ReferenceError' || e.type === 'not_defined');
+        if (isTplError) {
+          var undefinedScopeVarName = _.isArray(e.arguments) && e.arguments[0];
+          var output = {
+            undefinedScopeVarName: undefinedScopeVarName || '',
+            message:
+              undefinedScopeVarName ?
+              util.format('The variable `%s` is used in template "%s", but no value for `%s` was provided as template data.', undefinedScopeVarName, inputs.source, undefinedScopeVarName)
+              :
+              util.format('There is some unparseable or invalid syntax in template "%s". Details:\n', inputs.source, e.stack)
+          };
+          return exits.missingData(output);
+        }
+
+        // Unrecognized error
+        return exits.error(e);
+      }
+
+      try {
         // With lodash teplates, HTML entities are escaped by default.
         // Default assumption is we DON'T want that, so we'll reverse it.
         if (!inputs.escapeHTMLEntities) {
-          contents = _.unescape(contents);
+          result = _.unescape(result);
         }
-      } catch (e) {
-        var err = e;
-        if (!(typeof err === 'object' && err instanceof Error)){
-          err = new Error(err);
-        }
-
-        // Recognize lodash template error (scope variable not defined)
-        if (err.name === 'ReferenceError' || err.type === 'not_defined') {
-          var undefinedScopeVar = err.arguments && err.arguments[0];
-          err = {
-            status: 500,
-            exit: 'missingData',
-            name: 'ReferenceError',
-            message: util.format('`%s` is used in template "%s", but no value for `%s` was provided as template data.', undefinedScopeVar, inputs.source, undefinedScopeVar)
-          };
-          return (exits.missingData||exits.error)(err);
-        }
-
+      }
+      catch (e) {
         return exits.error(e);
       }
 
       // Finally, write templated string to disk
       writeFileFromStr({
         force: inputs.force,
-        string: contents,
+        string: result,
         destination: inputs.destination
       })
       .exec(exits);
