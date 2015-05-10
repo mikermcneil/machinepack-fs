a72ced726d1b0532a2bfc02152807c9f8e5de9d3
Mike McNeil <mike@balderdash.co>
2015-05-10 11:03:10 -0500

    Add includeHidden input

--- machines/ls.js
+++ machines/ls.js
@@ -24,29 +24,36 @@ module.exports = {
       friendlyName: 'Max depth',
       description: 'The maximum number of "hops" (i.e. directories deep) to include directory contents from.',
       extendedDescription: 'For instance, if you are running `ls` on "foo/" which has a subdirectory "foo/bar/baz/", if `depth` is set to 2, the results will include "foo/bar/baz/", but none of the files/folders contained within.',
-      example: 2,
-      defaultsTo: 2
+      example: 1,
+      defaultsTo: 1
     },
 
     includeFiles: {
-      friendlyName: 'Include files?',
-      description: 'Whether or not to include files in result array.',
+      friendlyName: 'Files?',
+      description: 'Whether or not to include files',
       example: true,
       defaultsTo: true
     },
 
     includeDirs: {
-      friendlyName: 'Include directories?',
-      description: 'Whether or not to include directories in result array.',
+      friendlyName: 'Directories?',
+      description: 'Whether or not to include directories',
       example: true,
       defaultsTo: true
     },
 
     includeSymlinks: {
-      friendlyName: 'Include symlinks?',
-      description: 'Whether or not to include symbolic links in result array.',
+      friendlyName: 'Symlinks?',
+      description: 'Whether or not to include symbolic links',
       example: true,
       defaultsTo: true
+    },
+
+    includeHidden: {
+      friendlyName: 'Hidden entries?',
+      description: 'Whether or not to include hidden files/directories/symlinks',
+      example: false,
+      defaultsTo: false
     }
 
   },
@@ -75,11 +82,18 @@ module.exports = {
     // for comparison later on.
     var topLvlDirDepth = inputs.dir.split(path.sep).length;
 
-    // Initialize the walker and teach it to skip directories that are
-    // deeper than requested.
+    // Initialize the walker and teach it to skip walking directories
+    // that are:
+    // • deeper than requested, or
+    // • hidden (if the `includeHidden` input is set to false)
     var walker = Walker(inputs.dir);
     walker.filterDir(function(dir, stat) {
+      // Too deep
       if (dir.split(path.sep).length > (topLvlDirDepth + inputs.depth)) {
+        return false;
+      }
+      // Too hidden
+      if (path.basename(dir).match(/^\./) && !inputs.includeHidden) {
         return false;
       }
       return true;
@@ -90,21 +104,39 @@ module.exports = {
     var results = [];
     if (inputs.includeFiles) {
       walker.on('file', function (entry, stat) {
-        // Filter out files that are deeper than requested
-        if (entry.split(path.sep).length <= (topLvlDirDepth + inputs.depth)) {
+        // Add the new entry to our result list unless it is:
+        //  • hidden (and the `includeHidden` input is set to false), or
+        //  • too deep
+        var tooHidden = path.basename(entry).match(/^\./) && !inputs.includeHidden;
+        var tooDeep = entry.split(path.sep).length > (topLvlDirDepth + inputs.depth);
+        if ( !tooHidden && !tooDeep  ) {
           results.push(entry);
         }
       });
     }
     if (inputs.includeDirs) {
       walker.on('dir', function (entry, stat) {
-        if (entry===inputs.dir) return;
+        // If this is the top-level directory, exclude it.
+        if (entry === inputs.dir) return;
+        // Add the new entry to our result list unless it is:
+        //  • hidden (and the `includeHidden` input is set to false)
+        var tooHidden = path.basename(entry).match(/^\./) && !inputs.includeHidden;
+        if ( !tooHidden ) {
+          results.push(entry);
+        }
         results.push(entry);
       });
     }
     if (inputs.includeSymlinks) {
       walker.on('symlink', function (entry, stat) {
+        // If this is the top-level directory, exclude it.
         if (entry===inputs.dir) return;
+        // Add the new entry to our result list unless it is:
+        //  • hidden (and the `includeHidden` input is set to false)
+        var tooHidden = path.basename(entry).match(/^\./) && !inputs.includeHidden;
+        if ( !tooHidden ) {
+          results.push(entry);
+        }
         results.push(entry);
       });
     }
