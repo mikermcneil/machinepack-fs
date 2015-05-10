e24d96ee3b2cafcbf3cb2384683cf34ba667b70a
Mike McNeil <mike@balderdash.co>
2015-05-10 11:12:42 -0500

    Fix error introduced in read-json.

--- machines/read-json.js
+++ machines/read-json.js
@@ -57,7 +57,7 @@ module.exports = {
     var MPJSON = require('machinepack-json');
     var thisPack = require('../');
 
-    thisPack.readFile({
+    thisPack.read({
       source: inputs.source
     }).exec({
       error: exits.error,
