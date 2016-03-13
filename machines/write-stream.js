a1e1ee81c00a79e2e8b0bf8ce3e257335883acf0
Mike McNeil <mike@balderdash.co>
2016-03-13 10:20:19 -0500

    Handled override conflicts in writeStream (force/alreadyExists), and added support for overwriting directories (potentially w/ nested contents) w/ files or a new directory.

--- machines/write-stream.js
+++ machines/write-stream.js
@@ -38,6 +38,10 @@ module.exports = {
       description: 'The stream was pumped to disk at the speciifed path.'
     },
 
+    alreadyExists: {
+      description: 'Something already exists at the specified path (overwrite by enabling the `force` input)'
+    },
+
   },
 
 
@@ -49,8 +53,9 @@ module.exports = {
   //   error: function (err){console.error('Error getting readable stream:',err);},
   //   success: function(stream) {
   //     require('./').writeStream({
-  //       destination: './experiment',
-  //       sourceStream: stream
+  //       destination: './experiment.md',
+  //       sourceStream: stream,
+  //       force: true
   //     }).exec(console.log);
   //   }
   // });
@@ -67,44 +72,62 @@ module.exports = {
     // Ensure path is absolute (resolve from cwd if not).
     inputs.destination = path.resolve(inputs.destination);
 
-    // Build the write drain (i.e. "write stream").
-    // This is where the content will go.
-    var writeDrain = fsx.createOutputStream(inputs.destination);
-
-    // Pipe the source stream down the drain.
-    //
-    // Note that since we're using `createOutputStream()`, the necessary
-    // directory hierarchy leading to the destination will be created if
-    // it does not already exist.
-    inputs.sourceStream.pipe(writeDrain);
-
-    // Handle write errors.
-    // (note that errors on the source Readable stream are the
-    //  responsibility of the provider of said stream)
-    var hasAlreadyCalledExit;
-    writeDrain.on('error', function (err){
-      // If we have not called one of our exits yet, then this is
-      // our first error.  So we'll handle it by calling the
-      // appropriate exit.
-      if (!hasAlreadyCalledExit) {
-        hasAlreadyCalledExit = true;
-        return exits.error(err);
+    // Only override an existing file if `inputs.force` is true.
+    fsx.exists(inputs.destination, function(exists) {
+      if (exists && !inputs.force) {
+        return exits.alreadyExists();
       }
-      // Otherwise this is a subsequent error.  Since we've already
-      // called an exit, there is not a whole lot else we can do.
-      // So we just ignore it.
-      // (we need to keep this handler bound or an error will be
-      //  thrown, potentially crashing the process)
-    });
 
-    // When finished...
-    writeDrain.once('finish', function (){
-      if (hasAlreadyCalledExit) { return; }
-      hasAlreadyCalledExit = true;
-      return exits.success();
-    });
+      // Delete existing files and/or directories if necessary.
+      (function _deleteExistingFilesAndOrDirsIfNecessary(next) {
+        if (!exists) {
+          return next();
+        }
+        else {
+          fsx.remove(inputs.destination, next);
+        }
+      })(function afterwards(err){
+        if (err) { return exits(err); }
+
+        // Build the write drain (i.e. "write stream").
+        // This is where the content will go.
+        var writeDrain = fsx.createOutputStream(inputs.destination);
+
+        // Pipe the source stream down the drain.
+        //
+        // Note that since we're using `createOutputStream()`, the necessary
+        // directory hierarchy leading to the destination will be created if
+        // it does not already exist.
+        inputs.sourceStream.pipe(writeDrain);
+
+        // Handle write errors.
+        // (note that errors on the source Readable stream are the
+        //  responsibility of the provider of said stream)
+        var hasAlreadyCalledExit;
+        writeDrain.on('error', function (err){
+          // If we have not called one of our exits yet, then this is
+          // our first error.  So we'll handle it by calling the
+          // appropriate exit.
+          if (!hasAlreadyCalledExit) {
+            hasAlreadyCalledExit = true;
+            return exits.error(err);
+          }
+          // Otherwise this is a subsequent error.  Since we've already
+          // called an exit, there is not a whole lot else we can do.
+          // So we just ignore it.
+          // (we need to keep this handler bound or an error will be
+          //  thrown, potentially crashing the process)
+        });
+
+        // When finished...
+        writeDrain.once('finish', function (){
+          if (hasAlreadyCalledExit) { return; }
+          hasAlreadyCalledExit = true;
+          return exits.success();
+        });
+      });//</after _deleteExistingFilesAndOrDirsIfNecessary>
+    });//</fsx.exists()>
   },
-
 
 
 };
