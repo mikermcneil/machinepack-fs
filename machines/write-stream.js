302420b171116a7c5f7f9f3e6545d5c607f2adae
Mike McNeil <git+mikermcneil@sailsjs.com>
2017-11-26 18:30:03 -0600

    Set up a fix for naughty streams.

--- machines/write-stream.js
+++ machines/write-stream.js
@@ -104,6 +104,9 @@ module.exports = {
       })(function afterwards(err){
         if (err) { return exits(err); }
 
+        // Declare a variable to act as a spinlock.
+        var hasAlreadyCalledExit;
+
         // Build the write drain (i.e. "write stream").
         // This is where the content will go.
         var writeDrain = fsx.createWriteStream(inputs.destination);
@@ -120,18 +123,39 @@ module.exports = {
         // • https://github.com/mikermcneil/machinepack-http/commit/6097c788cf484438b309d9925a56b701f4510a29
         // • https://github.com/sailshq/machinepack-strings/commit/0fd94b1603834503554888becd1965583b93ac04
         //
-        // But we managed to get around it without having to change anything in
-        // stream consumer methods, by implementing the fix suggested here:
+        // On one hand, it seems possible to get around this without having to change
+        // anything in stream consumer methods, by implementing the fix suggested here:
         // • https://github.com/request/request/issues/887#issuecomment-347050137
+        //
+        // But unfortunately, that doesn't work.  At least not for the `request` pkg.
+        // So instead we do two things here:
+        // 1. Treat any read stream errors as fatal so they're actually visible on the
+        //    console (carefully managing our spinlocks along the way).
+        // 2. Don't use the native `.pipe()`-- this at least prevents the naughty
+        //    `on('pipe')` code in `request` from triggering.
         // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-        inputs.sourceStream.pipe(writeDrain);
+        // (Note: The $-prefixed functions are standalone declarations because we use
+        // references to them again below as we clean up.)
+        var $onReadError;
+        $onReadError = function(err) {
+
+          // If we have not called one of our exits yet, then this is
+          // our first error.  So we'll handle it by calling the
+          // appropriate exit.
+          if (!hasAlreadyCalledExit) {
+            // Set the spinlock and leave through the `error` exit.
+            hasAlreadyCalledExit = true;
+            inputs.sourceStream.removeListener('error', $onReadError);
+            return exits.error(err);
+          }
+        };
+        inputs.sourceStream.on('error', $onReadError);//Œ
 
         // Handle write errors.
-        // Note that errors on the source Readable stream are the
-        // responsibility of the provider of said stream.
-        //
-        // Declare a variable to act as a spinlock.
-        var hasAlreadyCalledExit;
+        // > Note that errors on the source Readable stream are the
+        // > responsibility of the provider of said stream.  However,
+        // > we still have to take care of them for reasons mentioned
+        // > above.  Fortunately, we've already done that!  (see above)
         writeDrain.on('error', function (err){
           // If we have not called one of our exits yet, then this is
           // our first error.  So we'll handle it by calling the
@@ -139,6 +163,7 @@ module.exports = {
           if (!hasAlreadyCalledExit) {
             // Set the spinlock and leave through the `error` exit.
             hasAlreadyCalledExit = true;
+            inputs.sourceStream.removeListener('error', $onReadError);
             return exits.error(err);
           }
           // Otherwise this is a subsequent error.  Since we've already
@@ -146,7 +171,7 @@ module.exports = {
           // So we just ignore it.
           // We need to keep this handler bound or an error will be
           // thrown, potentially crashing the process.
-        });
+        });//Œ
 
         // When finished...
         writeDrain.once('finish', function (){
@@ -154,8 +179,16 @@ module.exports = {
           if (hasAlreadyCalledExit) { return; }
           // Otherwise set the spinlock and leave through the `success` exit.
           hasAlreadyCalledExit = true;
+          inputs.sourceStream.removeListener('error', $onReadError);
           return exits.success();
-        });
+        });//Œ
+
+
+
+        // Now actually pipe the thing:
+        inputs.sourceStream.pipe(writeDrain);
+        // TODO: do it raw instead
+
       });//</after _deleteExistingFilesAndOrDirsIfNecessary>
     });//</Filesystem.exists()>
   },
